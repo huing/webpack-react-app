500a3f60eafd9a5971a9e51d62c3231bd1937cc4
sci7 <747541102@qq.com>
2019-03-25 17:48:39 +0800

    create-react-app eject

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import {Button, DatePicker} from 'antd'
+import './App.css'
+import './index.styl'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <div>
+            <div>
+              <Button type="primary">antd测试</Button>
+            </div>
+            <div className="long">
+              <DatePicker />
+            </div>
+          </div>
+          
+          
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -21,8 +33,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
