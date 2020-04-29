4d43588d934269bc935ca766b7c6e21cecbf711d
xiaoxi <747541102@qq.com>
2020-04-29 20:37:38 +0800

    feat: add

--- src/api/home.js
+++ src/api/home.js
@@ -1,11 +1,11 @@
 import ioContext from './io-context'
 
-const Hello = ioContext.post('/users')
+const findOrderList = ioContext.post('/order/findOrderList')
 const Cats = ioContext.get('/cats')
 const CatsDetail = ioContext.get('/cats/detail')
 
 export default {
-  Hello,
+  findOrderList,
   Cats,
   CatsDetail,
 }
--- src/page-home/store.js
+++ src/page-home/store.js
@@ -3,7 +3,26 @@ import BaseStore from '@components/BaseTable/store'
 import Api from '../api'
 
 class Store extends BaseStore {
-  $listApi = Api.Hello
+  $listApi = Api.findOrderList
+  $ignoreParams = [
+    'search_type',
+    'keywords',
+  ]
+  $getParams = () => {
+    const {
+      search_type,
+      keywords,
+      created_at,
+      delivery_at,
+      sort_rule,
+      cityCode,
+      ...rest
+    } = this.$params
+    return {
+      [search_type]: keywords,
+      ...rest
+    }
+  }
   @observable detail = {}
   @action init = () => {
     this.getDetail()
