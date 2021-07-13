e65007b5f77e67e660c8f0130f116da2f1977270
zwh <zwh@hzddyx.com>
2021-07-13 18:37:19 +0800

    test

---
+++ test/test0053.js
@@ -0,0 +1,25 @@
+// https://leetcode-cn.com/problems/maximum-subarray/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let pre = 0,
+    maxAns = nums[0];
+  nums.forEach((x) => {
+    pre = Math.max(pre + x, x);
+    maxAns = Math.max(maxAns, pre);
+  });
+  return maxAns;
+};
+console.log(
+  maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+  //
+  maxSubArray([1]),
+  //
+  maxSubArray([0]),
+  //
+  maxSubArray([-1]),
+  //
+  maxSubArray([-100000])
+);
