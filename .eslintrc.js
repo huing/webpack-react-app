29e4d59ad9def38cae2ee4d09f4fc32a23f5ad55
zwh <zwh@hzddyx.com>
2021-08-10 13:56:30 +0800

    eslintrc prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// const eslintConfig = require('eslint-config-react-app');
+
+// console.log(eslintConfig)
+
+module.exports = {
+    extends: ['react-app'],
+    // extends: [...eslintConfig],
+};
